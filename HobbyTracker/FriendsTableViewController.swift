8bb0ae3a875c40e5584363ff342f66aef3e27058
Jessi <jessi@Fenando-6.local>
2020-05-21 23:51:08 -0500

    Finished setting up initial views

--- HobbyTracker/FriendsTableViewController.swift
+++ HobbyTracker/FriendsTableViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FriendsTableViewController.swift
 //  HobbyTracker
 //
 //  Created by Jessi on 5/21/20.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FriendsTableViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
